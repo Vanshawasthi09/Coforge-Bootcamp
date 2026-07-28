0b5ccb5411b44bf327c4788e263bddce6fa12e20
Vanshawasthi09 <awasthi.vansh54321@gmail.com>
2026-07-29 05:15:38 +0530

    Add files via upload

---
+++ SeleniumAutomationTesting/17-07-2026/Locators/src/test/java/LoactorsDemo/IDNameXpath.java
@@ -0,0 +1,33 @@
+package LoactorsDemo;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+
+public class IDNameXpath {
+	WebDriver driver;
+	String baseURL = "https://practicetestautomation.com/practice-test-login/";
+	@Test
+	public void IDLovator() {
+		driver = new ChromeDriver();
+		driver.navigate().to(baseURL);
+		driver.manage().window().maximize();
+  
+        
+		String expectedTitle = "Logged In Successfully | Practice Test Automation";
+		driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("Password123");
+		driver.findElement(By.xpath("//*[@id=\"submit\"]")).click();
+		
+		String actualTitle = driver.getTitle();
+		
+		if (expectedTitle.equals(actualTitle)) {
+            System.out.println("same title ");
+        }
+		else {
+			System.out.println("NOt correct output ");
+		}
+		
+	}
+}
